1028b8f7dc9d06a35da1495f9ecf5f9bf8dc9172
Luke Baltay <luke.baltay@yale.edu>
2022-07-05 15:37:46 -0400

    commit tree bff0c1ce810c88430faa1c7c09b6c49ab0651547

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+    # resources :products, only: [:index]
+    get '/', to: 'products#index', as: :products_path
+    post '/', to: 'products#add', as: :add_path
+
 end
